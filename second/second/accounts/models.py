843ef79c4cdd6b160052d17acf225746274703d4
purnishks28 <purnishks52@gmail.com>
2021-07-07 23:52:34 -0700

    my new project

--- second/second/accounts/models.py
+++ second/second/accounts/models.py
@@ -1,10 +1,5 @@
 from django.db import models
 # default --> null = False, blank = False
-GENDER_CHOICES = (
-    ('male', 'male'),
-    ('female', 'female'),
-    ('not specified','not specified'))
-
 class Person(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -14,9 +9,4 @@ class Person(models.Model):
     desc = models.TextField()
     salary = models.FloatField()
     is_verify = models.BooleanField(default=False)
-    gender = models.CharField(choices=GENDER_CHOICES,max_length=50,default='not specified')
-    documents = models.FileField(upload_to='documents/', default='default.jpg')
-    photo = models.ImageField(upload_to='photo/', null=True, blank=True)
 
-
-# Create your models here.
